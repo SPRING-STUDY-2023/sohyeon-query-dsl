6556ea4f8ee32b7909216dda40d4de8fc1f82e81
thguss <thgus345@naver.com>
2023-03-22 12:38:32 +0900

    Section4: 프로젝션과 결과 반환 - DTO 조회

--- src/test/java/study/querydsl/QueryDSLBasicTest.java
+++ src/test/java/study/querydsl/QueryDSLBasicTest.java
@@ -18,12 +18,14 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
-import com.querydsl.core.types.Expression;
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
+import study.querydsl.dto.MemberDto;
+import study.querydsl.dto.UserDto;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
@@ -483,4 +485,86 @@ public class QueryDSLBasicTest {
 			System.out.println("member => username = " + username + " age = " + age);
 		}
 	}
+
+	@Test
+	public void findDtoByJPQL() {
+		List<MemberDto> result = em.createQuery(
+				"select new study.querydsl.dto.MemberDto(m.username, m.age) from Member m", MemberDto.class)
+			.getResultList();
+
+		for (MemberDto memberDto : result) {
+			System.out.println("result: " + memberDto);
+		}
+	}
+
+	@Test
+	public void findDtoBySetter() {
+		List<MemberDto> result = queryFactory
+			.select(Projections.bean(MemberDto.class,
+				member.username,
+				member.age))
+			.from(member)
+			.fetch();
+
+		for (MemberDto memberDto : result) {
+			System.out.println("DTO: " + memberDto);
+		}
+	}
+
+	@Test
+	public void findDtoByField() {
+		List<MemberDto> result = queryFactory
+			.select(Projections.fields(MemberDto.class,
+				member.username,
+				member.age))
+			.from(member)
+			.fetch();
+
+		for (MemberDto memberDto : result) {
+			System.out.println("DTO: " + memberDto);
+		}
+	}
+
+	@Test
+	public void findDtoByConstruct() {
+		List<MemberDto> result = queryFactory
+			.select(Projections.constructor(MemberDto.class,
+				member.username,
+				member.age))
+			.from(member)
+			.fetch();
+
+		for (MemberDto memberDto : result) {
+			System.out.println("DTO: " + memberDto);
+		}
+	}
+
+	@Test
+	public void findUserDto() {
+		List<UserDto> result = queryFactory
+			.select(Projections.fields(UserDto.class,
+				member.username.as("name"),
+				member.age))
+			.from(member)
+			.fetch();
+
+		for (UserDto userDto : result) {
+			System.out.println("userDTO = " + userDto);
+		}
+	}
+
+	@Test
+	public void findUserDtoByConstructor() {
+		List<UserDto> result = queryFactory
+			.select(Projections.constructor(UserDto.class,
+				member.username,
+				member.age))
+			.from(member)
+			.fetch();
+
+		for (UserDto userDto : result) {
+			System.out.println("userDTO = " + userDto);
+		}
+	}
+
 }
