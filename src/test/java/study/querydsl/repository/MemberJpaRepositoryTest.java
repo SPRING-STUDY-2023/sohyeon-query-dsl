3ce9055e139d9fa7200f1343b7235e0eb8c1a5d6
thguss <thgus345@naver.com>
2023-03-25 15:41:32 +0900

    Section5: 조회 API 컨트롤러 개발

--- src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -10,6 +10,7 @@ import javax.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import study.querydsl.dto.MemberSearchCondition;
@@ -18,6 +19,7 @@ import study.querydsl.entity.Member;
 import study.querydsl.entity.Team;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Transactional
 class MemberJpaRepositoryTest {
 
