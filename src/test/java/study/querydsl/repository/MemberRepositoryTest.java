370130eeefd85b4a8af00177ed470cb662eeec2c
thguss <thgus345@naver.com>
2023-03-25 17:24:06 +0900

    Section7: 인터페이스 지원 - QuerydslPredicateExecutor

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -1,6 +1,7 @@
 package study.querydsl.repository;
 
 import static org.assertj.core.api.Assertions.*;
+import static study.querydsl.entity.QMember.*;
 
 import java.util.List;
 
@@ -17,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 @SpringBootTest
@@ -104,4 +106,28 @@ class MemberRepositoryTest {
 			.containsExactly("member1", "member2", "member3");
 	}
 
+	@Test
+	public void querydslPredicateExecutorTest() {
+		Team teamA = new Team("teamA");
+		Team teamB = new Team("teamB");
+		em.persist(teamA);
+		em.persist(teamB);
+
+		Member member1 = new Member("member1", 10, teamA);
+		Member member2 = new Member("member2", 20, teamA);
+		em.persist(member1);
+		em.persist(member2);
+
+		Member member3 = new Member("member3", 30, teamB);
+		Member member4 = new Member("member4", 40, teamB);
+		em.persist(member3);
+		em.persist(member4);
+
+		Iterable<Member> result = memberRepository.findAll(
+			member.age.between(10, 40).and(member.username.eq("member1")));
+
+		for (Member member : result) {
+			System.out.println("result: " + member);
+		}
+	}
 }
